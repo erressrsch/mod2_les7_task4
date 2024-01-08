5a04b74309bdb0fcd60b589c3182730cc1bb1acd
Szymon Chyc <erressrsch@gmail.com>
2024-01-08 23:10:16 +0100

    Add project files.

---
+++ mod2_les7_task4/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace mod2_les7_task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int year = 2016;
+            if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
+            {
+                Console.WriteLine($"{year} is a leap year");
+            }
+            else
+            {
+                Console.WriteLine($"{year} is not a leap year");
+            }
+        }
+    }
+}
+
